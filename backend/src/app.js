3ae0558cdbe6efab17860345b9c9c2a1e7bf56dd
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2024-12-17 15:38:45 +0530

    use absolute paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
 // const path = require("path");
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -22,7 +27,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // app.use(express.static("public"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
@@ -33,7 +38,7 @@ import { ApiError } from "./utils/ApiError.js";
 app.use("/api/v1/posts", postRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile('public/index.html', { root: '../' });
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.use(function (err, req, res, next) {
